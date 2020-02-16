6a159f69529cf14496f40dd1846325e50720290c
Richmond972 <60867464+Richmond972@users.noreply.github.com>
2020-02-16 01:12:21 +0000

    Add files via upload

---
+++ hw2Q1.m
@@ -0,0 +1,13 @@
+x=0:0.01:pi;
+
+N = 21;
+
+uN = zeros(1,length(x)); % dc component
+for n=1:N
+  uN = uN + (1/((2*n)-1)) * sin( ((2*n) -1)*x );
+end
+
+u = (4/pi) * uN;
+plot(x,u)
+xlabel('x')
+ylabel('u(x)')
